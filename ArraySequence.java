b306076d70c18b382527cfb5d1348013d3ca645c
Brandon OConnor <boconnor20@stuy.edu>
2020-12-08 11:44:56 -0500

    added first constructor and methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,19 +4,35 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
+  //Constructors
   public ArraySequence(int [] other){
+    data = new int[other.length];
     for (int i=0; i<other.length; i++){
       data[i]=other[i];
+      currentIndex=0;
     }
   }
+  //Methods
   public boolean hasNext(){
+    return currentIndex<data.length;
     }
   public int next(){
-
+    int beforenext = data[currentIndex];
+    try{
+      if(currentIndex>=data.length){
+        throw new NoSuchElementException("Current can't be greater than end");
+      }
+      currentIndex++;
+    }
+    catch(NoSuchElementException e){
+    throw new NoSuchElementException("Current can't be greater than end");
+    }
+   return beforenext;
   }
   public int length(){
+    return data.length;
       }
   public void reset(){
-
+    currentIndex=0;
     }
 }
--- StevenTesterA.java
+++ StevenTesterA.java
@@ -2,6 +2,7 @@ import java.util.NoSuchElementException;
 
 public class StevenTesterA{
     public static void main(String args[]){
+      /*
         IntegerSequence a = new Range(10,15);
         IntegerSequence b = new Range(1,15);
         IntegerSequence c = new Range(-30,15);
@@ -40,5 +41,14 @@ public class StevenTesterA{
             if(r.hasNext())str+=", ";
         }
         return str;
+        */
+        int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
     }
+    }
+
 }
