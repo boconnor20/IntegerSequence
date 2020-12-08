752a7dccd6fd559de250cfe536897bb4de7672f8
Brandon OConnor <boconnor20@stuy.edu>
2020-12-07 23:35:07 -0500

    added Ranged try and catch

--- Range.java
+++ Range.java
@@ -1,9 +1,26 @@
 import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
-  public Range(int start,  int end){  }
-  public void reset(){  }
-  public int length(){  }
+
+  public Range(int start,  int end){
+  try{
+      if(start>end){
+        throw new IllegalArgumentException("Start value must be less than or equal to end");
+      }
+      this.start=start;
+      this.end=end;
+      current = start;
+    }
+    catch(IllegalArgumentException e){
+      throw new IllegalArgumentException("Start value must be less than or equal to end");
+    }
+  }
+  public void reset(){
+    current=start;
+    }
+  public int length(){
+    return end-start+1;
+    }
   public boolean hasNext(){  }
 
   //@throws NoSuchElementException
